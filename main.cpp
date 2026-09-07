d1a32739cebe7d1ed680d7e2feda691c1fbd6aeb
Jaysiizzlle <Jaylenchae@gmail.com>
2026-09-07 05:10:24 +0000

    Added Link to video

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 
-// Practice 2 — Your Name
+// Practice 2 — Jaylen
 // CIS 5 Week 02 · Structure polish
 
+// Using the  for std:: for short cut
+  using std::cout; 
+
+// int main is the start of my program
 int main() {
-  // Block 1 — who you are (from Lab 2)
-  std::cout << "=== About me ===\n";
-  // TODO: 3+ labeled lines
-
+  //  These are my outputs "cout" that'll be sent into the terminal
+  cout << "=== About me ===\n";
+  cout << "Name: Jaylen Chae\n";
+  cout << "Interest: Phot`ography and Football\n";
+  cout << "CIS 5 goal: Learn C++ and hopefully apply it to learn other coding languages\n";
+  cout << "Tool path: GitHUB Codespace\n";
+  cout << "Fun fact: This is the first time I'm learning how to code\n";
+ 
   // Block 2 — this week's goals (from outcomes)
-  std::cout << "=== Week 02 goals ===\n";
-  // TODO: list 3 goals as complete sentences
+  cout << "=== Week 02 goals ===\n";
+  cout << "I can know difference between cin and cout\n";
+  cout << "I can learn how to use in the insertion operator << and extraction operator >>\n";
+  cout << "I know where the program starts from main() and ends to return 0;\n";
 
+  // return 0; is the exit of the program
   return 0;
 }
